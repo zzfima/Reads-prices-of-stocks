b93c0fa50a30b59228502410997e338170c71005
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2023-03-12 20:25:51 +0200

    rfct

--- StockPricesAnalysis/Client/Program.cs
+++ StockPricesAnalysis/Client/Program.cs
@@ -2,12 +2,25 @@
 using Service.Interfaces;
 using Service.StockPriceReaders;
 
-var stockSourceParser = StockSourceParserFactory.GetStockSourceParser(StockReaderType.Json);
+//Example read different sources
+/*
+IStockSourceParser stockSourceParser = StockSourceParserFactory.GetStockSourceParser(StockReaderType.Csv);
 IStockReader stockReader = new StocksReader(stockSourceParser);
-Task.Run(() => stockReader.StartReadAsync("JsonSource.json", new TimeSpan(0, 0, 1)));
+Task.Run(() => stockReader.StartReadAsync("CsvSource.csv", new TimeSpan(0, 0, 1)));
+*/
 
-Thread.Sleep(10000);
+/*
+IStockSourceParser stockSourceParser = StockSourceParserFactory.GetStockSourceParser(StockReaderType.Csv);
+IStockReader stockReader = new StocksReader(stockSourceParser);
+Task.Run(() => stockReader.StartReadAsync("CsvSource.csv", new TimeSpan(0, 0, 1)));
+*/
 
-Task.Run(() =>stockReader.StopReadAsync());
+IStockSourceParser stockSourceParser = StockSourceParserFactory.GetStockSourceParser(StockReaderType.Json);
+IStockReader stockReader = new StocksReader(stockSourceParser);
+Task.Run(() => stockReader.StartReadAsync("JsonSource.json", new TimeSpan(0, 0, 20)));
+
+Thread.Sleep(30000);
+
+Task.Run(() => stockReader.StopReadAsync());
 
 Console.ReadLine();
